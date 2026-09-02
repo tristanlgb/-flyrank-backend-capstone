e55b44619bfd453c445c61ca23745bbeed82d34e
Tristan Lenz <112999910+tristanlgb@users.noreply.github.com>
2026-09-02 18:50:25 -0300

    chore: configure week 6 and 7 workspaces

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
-  { ignores: ['node_modules/**', 'coverage/**', 'week-05-scraper/output/**'] },
+  { ignores: ['node_modules/**', 'coverage/**', '**/dist/**', 'tmp/**', 'week-05-scraper/output/**'] },
   js.configs.recommended,
   {
     files: ['**/*.js'],
